88c17ebeee6889e69f9b1c8447584500047388a0
Clinton Sheppard <gar3ts@gmail.com>
2010-12-21 15:28:49 -0600

    graycode codegolf

---
+++ src/Scratch/Codegolf/Graycode/Tests.cs
@@ -0,0 +1,10 @@
+namespace Scratch.Codegolf.Graycode
+{
+    /// <summary>
+    /// http://stackoverflow.com/questions/4119617/code-golf-gray-code
+    /// </summary>
+    public class Graycode
+    {
+        //static void Main(){long i=0,s=1<<int.Parse(Console.ReadLine());while(i<s)Console.WriteLine(Convert.ToString(s+i^i++/2,2).Remove(0,1));}
+    }
+}
