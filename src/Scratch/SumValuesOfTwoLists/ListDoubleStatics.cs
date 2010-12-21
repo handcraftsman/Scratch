a17c72f5fa9c2616bfb3c5bd5661c479980fcaed
Clinton Sheppard <gar3ts@gmail.com>
2010-12-21 13:53:44 -0600

    sum values of two lists

---
+++ src/Scratch/SumValuesOfTwoLists/ListDoubleStatics.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scratch.SumValuesOfTwoLists
+{
+    /// <summary>
+    /// http://stackoverflow.com/questions/1190657/add-two-lists-of-different-length-in-c/1241495#1241495
+    /// </summary>
+    public static class ListDoubleStatics
+    {
+        public static IList<double> Sum(IList<double> a, IList<double> b)
+        {
+            var doubles = Enumerable.Range(0, Math.Max(a.Count, b.Count))
+            .Select(x => (a.Count > x ? a[x] : 0) + (b.Count > x ? b[x] : 0))
+            .ToList();
+            return doubles;
+        }
+    }
+}
---
+++ src/Scratch/SumValuesOfTwoLists/Tests.cs
@@ -0,0 +1,102 @@
+//  * **********************************************************************************
+//  * Copyright (c) Clinton Sheppard
+//  * This source code is subject to terms and conditions of the MIT License.
+//  * A copy of the license can be found in the License.txt file
+//  * at the root of this distribution. 
+//  * By using this source code in any fashion, you are agreeing to be bound by 
+//  * the terms of the MIT License.
+//  * You must not remove this notice from this software.
+//  * **********************************************************************************
+
+using System.Collections.Generic;
+
+using FluentAssert;
+
+using NUnit.Framework;
+
+namespace Scratch.SumValuesOfTwoLists
+{
+    /// <summary>
+    /// http://stackoverflow.com/questions/1190657/add-two-lists-of-different-length-in-c/1241495#1241495
+    /// </summary>
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Given_2_lists_of_equal_length()
+        {
+            var a = new List<double>
+                {
+                    3,
+                    4,
+                    5
+                };
+            var b = new List<double>
+                {
+                    1,
+                    2,
+                    3
+                };
+
+            var result = ListDoubleStatics.Sum(a, b);
+            result.Count.ShouldBeEqualTo(a.Count);
+            result[0].ShouldBeEqualTo(a[0] + b[0]);
+            result[1].ShouldBeEqualTo(a[1] + b[1]);
+            result[2].ShouldBeEqualTo(a[2] + b[2]);
+        }
+
+        [Test]
+        public void Given_2_lists_where_first_is_longer()
+        {
+            var a = new List<double>
+                {
+                    1,
+                    2,
+                    3,
+                    4,
+                    5
+                };
+            var b = new List<double>
+                {
+                    1,
+                    2,
+                    3
+                };
+
+            var result = ListDoubleStatics.Sum(a, b);
+            result.Count.ShouldBeEqualTo(a.Count);
+            result[0].ShouldBeEqualTo(a[0] + b[0]);
+            result[1].ShouldBeEqualTo(a[1] + b[1]);
+            result[2].ShouldBeEqualTo(a[2] + b[2]);
+            result[3].ShouldBeEqualTo(a[3]);
+            result[4].ShouldBeEqualTo(a[4]);
+        }
+
+        [Test]
+        public void Given_2_lists_where_second_is_longer()
+        {
+            var a = new List<double>
+                {
+                    1,
+                    2,
+                    3
+                };
+            var b = new List<double>
+                {
+                    1,
+                    2,
+                    3,
+                    4,
+                    5
+                };
+
+            var result = ListDoubleStatics.Sum(a, b);
+            result.Count.ShouldBeEqualTo(b.Count);
+            result[0].ShouldBeEqualTo(a[0] + b[0]);
+            result[1].ShouldBeEqualTo(a[1] + b[1]);
+            result[2].ShouldBeEqualTo(a[2] + b[2]);
+            result[3].ShouldBeEqualTo(b[3]);
+            result[4].ShouldBeEqualTo(b[4]);
+        }
+    }
+}
